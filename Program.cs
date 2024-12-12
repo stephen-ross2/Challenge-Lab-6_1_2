c76b344dc36a277a5b4d7b464ef65b120a765896
stephen-ross2 <stephen.ross2@outlook.com>
2024-12-12 13:51:42 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Challenge_Lab_6_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 3, 0, 1 };
+            int missing = MissingNumber(nums);
+            Console.WriteLine("The missing number is: " + missing);
+        }
+
+        static int MissingNumber(int[] nums)
+        {
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            foreach (int num in nums)
+            {
+                dict[num] = true;
+            }
+
+            
+            int n = nums.Length;
+            for (int i = 0; i <= n; i++)
+            {
+                if (!dict.ContainsKey(i))
+                {
+                    return i; 
+                }
+            }
+
+            return -1;
+        }
+    }
+
+}
+ 
